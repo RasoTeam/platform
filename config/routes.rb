ec0cc7e9f1c12cdbe5bc00c1231d5243fda9fac8
Cláudio Belo Lourenço <pt.smooke@gmail.com>
2012-11-14 16:09:46 +0000

    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Platform::Application.routes.draw do
+
+  resources :companies do
+    resources :users
+    resources :bills
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
