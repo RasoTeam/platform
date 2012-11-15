6673dde732f0272697aca8c5f79cfb006566c1f8
Tiago Emanuel Oliveira Gomes <tg.gms89@gmail.com>
2012-11-15 03:18:23 +0000

    Added bills and companies partials

--- app/controllers/super_users_controller.rb
+++ app/controllers/super_users_controller.rb
@@ -1,16 +1,28 @@
 class SuperUsersController < ApplicationController
 	def index
 		@companies = Company.all
+		@bills = Bill.all
 	end
 
 	def show
-		@companie = Company.find(params[:id])
+		@company = Company.find(params[:id])
 	end
 	
 	def edit
-		@companie = Company.find(params[:id])
+		@company = Company.find(params[:id])
 	end
-	
+
+	def update
+		@company = Company.find(params[:id])
+		@company.update_attributes(params[:id])
+
+		redirect_to company_path(@company)
+	end
+
 	def destroy
+		@company = Company.find(params[:id])
+		@company.destroy
+
+		redirect_to companies_path
 	end
 end
