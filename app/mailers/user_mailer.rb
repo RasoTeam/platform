46cea0eb566b2da2b2f7f85baf1cefd37901c7d2
Cláudio Belo Lourenço <pt.smooke@gmail.com>
2012-11-23 17:36:45 +0000

    modified:   Gemfile 	modified:   app/mailers/user_mailer.rb 	modified:   app/views/user_mailer/verification_email.html.erb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
   def verification_email(user)
     @user = user
-    mail( :to => user.email, :subject => "RASO SPAM")
+    mail( :to => user.email, :subject => "Account Confirmation")
   end
 end
