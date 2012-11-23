cc96410e248bf7fc1547790c42edd49d3cbafca7
Tiago Emanuel Oliveira Gomes <tg.gms89@gmail.com>
2012-11-23 17:53:34 +0000

    Rasocomp Permissions

--- app/controllers/rasocomp/job_offers_controller.rb
+++ app/controllers/rasocomp/job_offers_controller.rb
@@ -1,4 +1,5 @@
 class Rasocomp::JobOffersController < Rasocomp::ApplicationController
+  before_filter :manager_or_root
 
   #Listar todas as ofertas de trabalho de uma empresa
   def index
@@ -61,5 +62,14 @@ class Rasocomp::JobOffersController < Rasocomp::ApplicationController
 
     redirect_to company_job_offers_path
   end
+
+  private
+    def manager_or_root
+      comp = Company.find(params[:id])
+      unless manager_signed_in?(comp.tag) || root_signed_in?(comp.tag)
+        flash[:alert] = t(:no_permission_to_access) 
+        redirect_to company_signin_path(comp)
+      end
+    end
 
 end
