26fedf738c7cbf105cf6459f774e8184c54e3a1e
Tiago Emanuel Oliveira Gomes <tg.gms89@gmail.com>
2012-11-24 15:23:14 +0000

    modified:   app/controllers/rasocomp/bills_controller.rb 	modified:   app/controllers/rasocomp/job_offers_controller.rb 	modified:   app/controllers/rasocomp/users_controller.rb 	modified:   app/views/public/job_offers/new.html.erb 	modified:   app/views/rasocomp/users/show.html.erb

--- app/controllers/rasocomp/users_controller.rb
+++ app/controllers/rasocomp/users_controller.rb
@@ -1,7 +1,6 @@
 class Rasocomp::UsersController < Rasocomp::ApplicationController
-  before_filter :manager_or_root, :only => [:new, :create]
+  before_filter :manager_or_root, :only => [:new, :create, :index]
   before_filter :manager_or_user_self, :only => [:show, :edit, :update, :dashboard]
-  before_filter :manager, :only => [:index]
 
   def show
     @company = Company.find(params[:company_id])
@@ -69,15 +68,6 @@ class Rasocomp::UsersController < Rasocomp::ApplicationController
     def manager_or_user_self
       comp = Company.find(params[:company_id])
       unless manager_signed_in?(comp.tag) || (user_signed_in?(comp.tag) && current_user(comp.tag).id == Integer(params[:id]))
-        flash[:alert] = t(:no_permission_to_access) 
-        redirect_to company_signin_path(comp)
-      end
-    end
-
-  private
-    def manager
-      comp = Company.find(params[:company_id])
-      unless manager_signed_in?(comp.tag)
         flash[:alert] = t(:no_permission_to_access) 
         redirect_to company_signin_path(comp)
       end
