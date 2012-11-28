15561e48b144060938d65b37bec8762f79200f5d
Filipe Liu <filipeliu@hotmail.com>
2012-11-28 14:31:48 +0000

    credits fix

--- app/controllers/rasocomp/time_offs_controller.rb
+++ app/controllers/rasocomp/time_offs_controller.rb
@@ -17,6 +17,11 @@ class Rasocomp::TimeOffsController < Rasocomp::ApplicationController
     @company = Company.find( params[:company_id])
     @user = @company.users.find( params[:user_id])
     @timeoff = @user.time_offs.find( params[:id])
+    if @timeoff.state == 2
+      user_t = User.find( @timeoff.user_id)
+      user_t.time_off_days -= @timeoff.days
+      user_t.save!(:validate => false)
+    end
     @timeoff.update_attribute :state, 1
     @timeoff.update_attribute :color, '#33FF33'
     redirect_to manage_company_user_time_offs_path( @company, @user)
@@ -24,12 +29,17 @@ class Rasocomp::TimeOffsController < Rasocomp::ApplicationController
   end
 
   def disapprove
-    @company = Company.find( params[:company_id])
-    @user = @company.users.find( params[:user_id])
-    @timeoff = @user.time_offs.find( params[:id])
-    @timeoff.update_attribute :state, 2
-    @timeoff.update_attribute :color, '#330000'
-    redirect_to manage_company_user_time_offs_path( @company, @user)
+    company = Company.find( params[:company_id])
+    user = company.users.find( params[:user_id])
+    timeoff = user.time_offs.find( params[:id])
+    if timeoff.state != 2
+      user_t = User.find( timeoff.user_id)
+      user_t.time_off_days += timeoff.days
+      user_t.save!(:validate => false)
+    end
+    timeoff.update_attribute :state, 2
+    timeoff.update_attribute :color, '#330000'
+    redirect_to manage_company_user_time_offs_path( company, user)
     #redirect_to root_path
   end
 
@@ -69,8 +79,10 @@ class Rasocomp::TimeOffsController < Rasocomp::ApplicationController
   def destroy
     user = User.find( params[:user_id])
     timeoff = TimeOff.find( params[:id])
-    user.time_off_days += timeoff.days
-    user.save!(:validate => false)
+    if timeoff.state != 2 #only return credits if pending (0) or approved (1)
+      user.time_off_days += timeoff.days
+      user.save!(:validate => false)
+    end
     timeoff.destroy
     redirect_to company_user_time_offs_path( params[:company_id], params[:user_id])
   end
--- config/environment.rb
+++ config/environment.rb
@@ -8,10 +8,12 @@ Platform::Application.initialize!
 #config.action_mailer.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
-  :port => 587,
+  :port => 25,
   :domain => "gmail.com",
-  :user_name => "jonhdeliver@gmail.com",
-  :password => "Q1e3t5u7",
+#  :user_name => "jonhdeliver@gmail.com",
+#  :password => "Q1e3t5u7",
+  :user_name => "rasodeliver@gmail.com",
+  :password => "qetuoadgjl",
   :authentication => "plain",
   :enable_starttls_auto => true
 }
