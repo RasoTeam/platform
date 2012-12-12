d24066745fb3a23cc303ebf6efae1aa520d8db81
Samuel Almeida <sam.alm321@gmail.com>
2012-12-12 10:43:07 +0000

    adds changes to frontoffice and companies spec

--- spec/requests/frontoffices_spec.rb
+++ spec/requests/frontoffices_spec.rb
@@ -9,15 +9,13 @@ describe "Frontoffices" do
     it { should have_content('our idea') }
     it { should have_content('contacts') }
     it { should have_content('blog') }
-    it { should have_content('register company') }
-		it { should have_content('sign in') }
 
 	  it { should have_link('about us', href: aboutus_path) }
 	  it { should have_link('our idea', href: idea_path) }
 	  it { should have_link('contacts', href: contacts_path) }
 	  it { should have_link('blog', href: 'http://rasoblog.herokuapp.com/') }
-	  it { should have_link('register company', href: new_public_company_path) }
-	  it { should have_link('sign in', href: company_signin_path) }
+  
+
   end
 
     
@@ -25,19 +23,22 @@ describe "Frontoffices" do
  		before { visit homefront_path }
 
     it_should_behave_like "all static pages"
-    it { should have_selector('title', text: 'RasoHR') }
+    it { should have_selector('title', text: 'Raso') }
+    it { should have_link('Try it Free!', href: new_public_company_path) }
   end
 
   describe "About us" do
   	before {visit aboutus_path}
 
   	it_should_behave_like "all static pages"
+    it { should have_content('Meet the Team') }
   	it {should have_selector('title', text: 'RasoHR | About us')}
   end
 
   describe "Our idea" do
   	before {visit idea_path}
 
+    it { should have_content('Simple') }
   	it_should_behave_like "all static pages"
   	it {should have_selector('title', text: 'RasoHR | Our idea')}
   end
@@ -45,6 +46,7 @@ describe "Frontoffices" do
   describe "Contacts" do
   	before {visit contacts_path}
 
+    it { should have_content('Get in Touch') }
   	it_should_behave_like "all static pages"
   	it {should have_selector('title', text: 'RasoHR | Contacts')}
   end
