70b8dffcac63164cb20cf5696526bb1188a973b3
Filipe Liu <filipeliu@hotmail.com>
2012-12-12 10:11:11 +0000

    add users courses

--- app/controllers/rasocomp/courses_controller.rb
+++ app/controllers/rasocomp/courses_controller.rb
@@ -28,7 +28,27 @@ class Rasocomp::CoursesController < ApplicationController
     @training = Training.find( params[:training_id])
     @course = Course.find( params[:id])
   end
+  
+  def update
+    @company = Company.find( params[:company_id])
+    @training = @company.trainings.find( params[:training_id])
+    @course = Course.find( params[:id])
+    if @course.update_attributes(params[:course])
+      flash[:success] = t(:successful_update)
+      redirect_to company_trainings_path @company
+    else
+      render 'edit'
+    end
+  end
 
+  def update_users
+    @company = Company.find( params[:company_id])
+    @training = @company.trainings.find( params[:training_id])
+    @course = Course.find( params[:id])
+    @lol = "tru dat"
+    render 'edit'
+  end
+  
   def create
     @company = Company.find( params[:company_id])
     @training = @company.trainings.find( params[:training_id])
