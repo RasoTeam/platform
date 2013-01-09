80ccf8facf719adf833a554c3cd99f6868585bbe
CarlosMOGoncalves <carlosmoliveiragoncalves@gmail.com>
2013-01-09 17:38:22 +0000

    Modulo Avaliacoes: Update 3 - Alteracoes de Funcionalidade

--- app/controllers/rasocomp/parameters_controller.rb
+++ app/controllers/rasocomp/parameters_controller.rb
@@ -8,18 +8,27 @@ class Rasocomp::ParametersController < Rasocomp::ApplicationController
   def new
     @parameter = Parameter.new
     @company = Company.find(params[:company_id])
+
+    respond_to do |format|
+          format.html # new.html.erb
+          format.js
+    end
   end
 
   def create
     @parameter = Parameter.new(params[:parameter])
     @company_id = params[:company_id]
 
-    if @parameter.save
-      flash[:success] = "Evaluation Parameter successfully added."
-      redirect_to new_company_evaluation_path(@company_id)
-    else
-      flash[:error] = "Could not save parameter"
-      redirect_to company_parameters_path(@company_id)
+    respond_to do |format|
+      if @parameter.save
+        format.js
+        format.html{ flash[:success] = "Evaluation Parameter successfully added."
+                     redirect_to new_company_evaluation_path(@company_id) }
+      else
+        format.html{flash[:error] = "Could not save parameter"
+                    redirect_to company_parameters_path(@company_id)}
+        format.js{}
+      end
     end
 
   end
