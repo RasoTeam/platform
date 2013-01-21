511b686ed75f3461c26f35960ecc9056b4ff9915
CarlosMOGoncalves <carlosmoliveiragoncalves@gmail.com>
2013-01-21 12:04:24 +0000

    Update geral

--- app/controllers/rasocomp/parameters_controller.rb
+++ app/controllers/rasocomp/parameters_controller.rb
@@ -9,6 +9,7 @@ class Rasocomp::ParametersController < Rasocomp::ApplicationController
     @parameter = Parameter.new
     @company = Company.find(params[:company_id])
 
+    puts "PASSO POR AQUI"
     respond_to do |format|
           format.html # new.html.erb
           format.js
@@ -19,15 +20,20 @@ class Rasocomp::ParametersController < Rasocomp::ApplicationController
     @parameter = Parameter.new(params[:parameter])
     @company_id = params[:company_id]
 
-    respond_to do |format|
-      if @parameter.save
+    puts "POR AQUI TAMBEM"
+    if @parameter.save
+      respond_to do |format|
         format.js
-        format.html{ flash[:success] = "Evaluation Parameter successfully added."
-                     redirect_to new_company_evaluation_path(@company_id) }
-      else
+        format.html{
+            flash[:success] = "Evaluation Parameter successfully added."
+            redirect_to new_company_evaluation_path(@company_id)
+        }
+      end
+    else
+      respond_to do |format|
         format.html{flash[:error] = "Could not save parameter"
-                    redirect_to company_parameters_path(@company_id)}
-        format.js{}
+        redirect_to company_parameters_path(@company_id)}
+        format.js
       end
     end
 
